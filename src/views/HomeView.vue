ee5286a59cf5c7364aca8b4e7d072a73af87b06d
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-11-11 21:17:30 +0700

    initiate

---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mw-[1024px] mx-auto">
+    <div class="text-center text-4xl">Welcome to Pokemon workshop !</div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00aff0",
+          secondary: "#D926A9",
+          accent: "#1FB2A6",
+          neutral: "#191D24",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+      "light",
+    ],
+  },
+};
